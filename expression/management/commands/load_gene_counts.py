0fff06dc818065d335329c224f767863e75b6cbf
John Luff <127944305+42john42@users.noreply.github.com>
2025-06-19 10:45:47 +0100

    Correct file names in help text

--- expression/management/commands/load_gene_counts.py
+++ expression/management/commands/load_gene_counts.py
@@ -1,9 +1,9 @@
 from csv import DictReader
+
 from django.core.management import BaseCommand
 
-# Import the model 
+# Import the model
 from expression.models import Genecounts
-
 
 ALREDY_LOADED_ERROR_MESSAGE = """
 If you need to reload the child data from the CSV file,
@@ -22,25 +22,28 @@ python manage.py load_gene_counts
 
 class Command(BaseCommand):
     # Show this when the user types help
-    help = "Loads data from whole_genecounts.csv"
+    help = "Loads data from NormalisedGeneCounts.csv"
 
     def handle(self, *args, **options):
-    
         # Show this if the data already exist in the database
         if Genecounts.objects.exists():
-            print('gene counts data already loaded...exiting.')
+            print("gene counts data already loaded...exiting.")
             print(ALREDY_LOADED_ERROR_MESSAGE)
             return
-            
+
         # Show this before loading the data into the database
         print("Loading gene counts data")
 
-
-        #Code to load the data into database
-        for row in DictReader(open('./expression/files/NormalisedGeneCounts.csv')):
-            Genecount=Genecounts(sampleID=row['sampleID'], geneName=row['geneName'], counts=row['counts'], group=row['group'], sex = row['sex'])  
+        # Code to load the data into database
+        for row in DictReader(open("./expression/files/NormalisedGeneCounts.csv")):
+            Genecount = Genecounts(
+                sampleID=row["sampleID"],
+                geneName=row["geneName"],
+                counts=row["counts"],
+                group=row["group"],
+                sex=row["sex"],
+            )
             Genecount.save()
-    
+
     def __str__(self):
         return self.title
-            
--- expression/management/commands/load_transcript_counts.py
+++ expression/management/commands/load_transcript_counts.py
@@ -1,9 +1,9 @@
 from csv import DictReader
+
 from django.core.management import BaseCommand
 
-# Import the model 
+# Import the model
 from expression.models import Transcriptcounts
-
 
 ALREDY_LOADED_ERROR_MESSAGE = """
 If you need to reload the child data from the CSV file,
@@ -18,28 +18,31 @@ python manage.py load_transcript_counts
 
 class Command(BaseCommand):
     # Show this when the user types help
-    help = "Loads data from whole_genecounts.csv"
+    help = "Loads data from NormalisedTranscriptCounts.csv"
 
     def handle(self, *args, **options):
-    
         # Show this if the data already exist in the database
         if Transcriptcounts.objects.exists():
-            print('transcript counts already loaded...exiting.')
+            print("transcript counts already loaded...exiting.")
             print(ALREDY_LOADED_ERROR_MESSAGE)
             return
-            
+
         # Show this before loading the data into the database
         print("Loading transcript counts")
 
-
-        #Code to load the data into database
-        for row in DictReader(open('./expression/files/NormalisedTranscriptCounts.csv')):
-            Transcriptcount=Transcriptcounts(sampleID=row['sampleID'], geneName=row['geneName'], isoform=row['isoform'], counts=row['counts'], group=row['group'], sex = row['sex'])  
+        # Code to load the data into database
+        for row in DictReader(
+            open("./expression/files/NormalisedTranscriptCounts.csv")
+        ):
+            Transcriptcount = Transcriptcounts(
+                sampleID=row["sampleID"],
+                geneName=row["geneName"],
+                isoform=row["isoform"],
+                counts=row["counts"],
+                group=row["group"],
+                sex=row["sex"],
+            )
             Transcriptcount.save()
-    
+
     def __str__(self):
         return self.title
-
-
-
-

2c5627e97b1e3cd980651377bc73aa77aa3a5bdd
John Luff <127944305+42john42@users.noreply.github.com>
2025-06-26 15:40:02 +0100

    Set debug true for dev

--- isoVisDev/settings.py
+++ isoVisDev/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-t-n*(rbzl@29toox-%zh16z^$8jnslm4kddj*x0pazut#y9t!h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "isoforms-rse-env.eba-3f5qqpmj.eu-north-1.elasticbeanstalk.com",
